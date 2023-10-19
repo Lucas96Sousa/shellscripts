3df0ec4a33b97e6e26b349d370d9fd156edac728
Lucas Sousa <lucas996oliveira@gmail.com>
2023-10-18 22:22:18 -0300

    feat: finish aula02

--- aula02/aula02.sh
+++ aula02/aula02.sh
@@ -1,1 +1,33 @@
 #!/usr/bin/env bash
+
+# Uso básico das variáveis de ambiente
+: <<COMMENT
+echo "Qual a linguagem do sistema ?"
+echo -e "$LANG \n"
+
+echo "Qual o diretório do usuário logado ?"
+echo -e "$HOME \n"
+
+echo "Qual o nome do usuário logado ?"
+echo -e "$USER"
+COMMENT
+
+# Comando dentro das variáveis
+: <<COMMENT
+listagem=$(ls /boot)
+listagem_2=$(ls /home)
+
+echo -e "Listando '/boot'\n$listagem \n"
+echo -e "Listando '/home'\n$listagem_2 \n"
+COMMENT
+
+# Interagindo com o STDIN
+read -p "Qual o seu nome ? " nome
+
+read -p "Qual a sua altura ? " altura
+
+read -p "Qual a sua escolaridade ? " escolaridade
+
+echo -e "Seu nome é: $nome \n"
+echo -e "Sua altura é: $altura \n"
+echo -e "Sua escolaridade é: $escolaridade \n"
